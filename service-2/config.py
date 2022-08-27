f15a85924f4324ed464b9e09f2f54f8e164bc94a
Anna Hmyria <anyagmyrka@gmail.com>
2022-08-27 20:37:08 +0300

    Init commit

---
+++ service-2/config.py
@@ -0,0 +1,11 @@
+class DBConfig(object):
+    def __init__(self) -> None:
+        self.host = "localhost"
+        self.dbname = "my_db"
+        self.user = "annahmyria"
+        self.password = ""
+        # self.value = "val"
+
+class Config(object):
+    table_name = "mytable"
+    value = "value"
---
+++ service-2/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import pathlib
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Web App with Python Flask in servise-2\n"
+
+if __name__=="__main__":
+    app.run(debug=True, host='0.0.0.0')
